2e4be36414443a90d53222dd0261711024f6bd5f
James Wickett <wickett@gmail.com>
2013-09-17 12:19:13 -0500

    changed the pair.sh script to add a prompt for sudo pw on cleanup

--- pair.sh
+++ pair.sh
@@ -14,33 +14,42 @@ ROUTER='192.168.11.1'
 if [[ $1 = 'start' ]];
   then 
   # Download the public keys to the pair user
+  echo "Please provide the password for sudo"
   sudo bash -c "curl https://github.com/$2.keys 2>/dev/null >> ~pair/.ssh/authorized_keys"
+  sudo sed -i -e 's/^/command\=\"\/usr\/local\/bin\/tmux\ \-S\ \/tmp\/pairing\ attach\ \-t\ pairing\"\,no\-port\-forwarding\,no\-X11\-forwarding\ /' ~pair/.ssh/authorized_keys
   sudo chown pair:$GROUP /Users/pair/.ssh/authorized_keys
  
   # Punch a hole in the firewall
+  echo "Please provide the password for the router"
   ssh root@$ROUTER "iptables -t nat -I PREROUTING -p tcp --dport 2222 -j DNAT --to $LAN_IP:22 && iptables -I FORWARD -p tcp -d $LAN_IP --dport 22 -j ACCEPT" 
   # Copy the ssh command to the clipboard - OS X specific
   echo "give this to the pairer > ssh pair@$EXTERNAL_IP -p2222"
-  echo "tell them to run this > tmux -S /tmp/pairing attach -t pairing"
-  sleep 5
+  sleep 8
   
   # ************************** PAIRNG ****************************
   tmux -S /tmp/pairing new -ds pairing && chgrp $GROUP /tmp/pairing && tmux -S /tmp/pairing attach -t pairing
 
-elif [[ $1 = 'stop' ]];
-  then 
   # ************************** CLEANUP ***************************
   # Cleanup keys
+  echo "deleting the pairee keys, might need sudo password"
   sudo rm -f /Users/pair/.ssh/authorized_keys
+
+  # boot user off machine 
+  sudo kill -9 `who -u | grep pair | tr -d ' ' | cut -f2`
+
   # remove hole in the firewall
+  echo "Please provide the password for the router"
   ssh root@$ROUTER "iptables -t nat -D PREROUTING -p tcp --dport 2222 -j DNAT --to $LAN_IP:22 && iptables -D FORWARD -p tcp -d $LAN_IP --dport 22 -j ACCEPT" 
 
-  # boot user off machine
-  sudo kill -9 `who -u | grep pair | rev | cut -f1 -d" " | rev`
+  # one last chance to make sure the pair user didnt get back on before firewall rules went into affect
+  sudo kill -9 `who -u | grep pair | tr -d ' ' | cut -f2`
+  echo "Killing pairing session now, CTRL-c to save the session"
+  echo "..."
+  sleep 10
+  tmux -S /tmp/pairing kill-session -t pairing
 
 else
-  echo "Usage: pair.sh [start|stop] [github_id]"
+  echo "Usage: pair.sh [start] [github_id]"
 fi
 
 exit
-
